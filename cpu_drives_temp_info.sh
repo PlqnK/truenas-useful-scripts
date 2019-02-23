f114a991d94e8848d1058647b4c3eded99da0639
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-02-23 15:41:15 +0100

    Rework drive temperature recovery
    Add SSD and SAS drive support based on https://github.com/Spearfoot/FreeNAS-scripts/blob/f049158b582df0c705d569c236ce2cf071cc0a59/get_hdd_temp.sh#L117 and geraldh/FreeNAS-scripts@ee9bf6d7bf3583b5d4454acee6d7289a29bc13f7

--- cpu_drives_temp_info.sh
+++ cpu_drives_temp_info.sh
@@ -17,12 +17,20 @@ done
 echo ""
 
 for drive_label in ${SATA_DRIVES}; do
-  serial_number="$(smartctl -i /dev/"${drive_label}" | grep "Serial Number" | awk '{print $3}')"
-  drive_temperature="$(smartctl -A /dev/"${drive_label}" | grep "Temperature_Celsius" | awk '{print $10}')"
-  # Some drives don't report their temperature in their SMART.
-  if [[ "${drive_temperature}" == "" ]]; then
-    printf "%s %-15s: N/A\n" "${drive_label}" "${serial_number}"
-  else
-    printf "%s %-15s: %s°C\n" "${drive_label}" "${serial_number}" "${drive_temperature}"
+  drive_information_attributes="$(smartctl -i -A /dev/"${drive_label}")"
+  serial_number="$(echo "${drive_information_attributes}" | grep "Serial Number" | awk '{print $3}')"
+  drive_temperature="$(echo "${drive_information_attributes}" | grep "Temperature_Celsius" | awk '{print $10}')" # SATA HDD
+  if [[ -z "${drive_temperature}" ]]; then
+    drive_temperature="$(echo "${drive_information_attributes}" | grep "Airflow_Temperature_Cel" | awk '{print $10}')" # SATA SSD
+    if [[ -z "${drive_temperature}" ]]; then
+      drive_temperature="$(echo "${drive_information_attributes}" | grep "Current Drive Temperature" | awk '{print $4}')" # SAS HDD
+      if [[ -z "${drive_temperature}" ]]; then
+        drive_temperature="N/A" # Some drives don't report their temperature
+      fi
+    fi
   fi
+  if [[ "${drive_temperature}" != "N/A" ]]; then
+    drive_temperature="${drive_temperature}°C"
+  fi
+  printf "%s (%-15s): %s\n" "${drive_label}" "${serial_number}" "${drive_temperature}"
 done
