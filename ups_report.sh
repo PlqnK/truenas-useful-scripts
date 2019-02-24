6ff874120a5b5fdf16ef7d51ed660b2a4a3baa9f
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-02-24 16:33:41 +0100

    Reorganize variables

--- ups_report.sh
+++ ups_report.sh
@@ -16,48 +16,48 @@ readonly EMAIL_CONTENT="/tmp/ups_report.eml"
 ) > "${EMAIL_CONTENT}"
 
 for ups in ${UPS_LIST}; do
-  manufacturer="$(upsc "${ups}" device.mfr)"
   model="$(upsc "${ups}" device.model)"
+  manufacturer="$(upsc "${ups}" device.mfr)"
   serial_number="$(upsc "${ups}" device.serial)"
-  firmware_version="$(upsc "${ups}" ups.firmware)"
-  status="$(upsc "${ups}" ups.status)"
-  load="$(upsc "${ups}" ups.load)"
   uptime="$(upsc "${ups}" device.uptime)"
+  status="$(upsc "${ups}" ups.status)"
+  firmware_version="$(upsc "${ups}" ups.firmware)"
   temperature="$(upsc "${ups}" ups.temperature)"
-  beeper_status="$(upsc "${ups}" ups.beeper.status)"
-  timer_shutdown="$(upsc "${ups}" ups.timer.shutdown)"
-  timer_start="$(upsc "${ups}" ups.timer.start)"
+  load="$(upsc "${ups}" ups.load)"
   delay_shutdown="$(upsc "${ups}" ups.delay.shutdown)"
   delay_start="$(upsc "${ups}" ups.delay.start)"
+  timer_shutdown="$(upsc "${ups}" ups.timer.shutdown)"
+  timer_start="$(upsc "${ups}" ups.timer.start)"
   test_interval="$(upsc "${ups}" ups.test.interval)"
   last_test_result="$(upsc "${ups}" ups.test.result)"
   last_test_date="$(upsc "${ups}" ups.test.date)"
+  efficiency="$(upsc "${ups}" ups.efficiency)"
   power="$(upsc "${ups}" ups.power)"
   power_nominal="$(upsc "${ups}" ups.power.nominal)"
   real_power="$(upsc "${ups}" ups.realpower)"
   real_power_nominal="$(upsc "${ups}" ups.realpower.nominal)"
-  efficiency="$(upsc "${ups}" ups.efficiency)"
-  output_frequency="$(upsc "${ups}" output.frequency)"
-  output_frequency_nominal="$(upsc "${ups}" output.frequency.nominal)"
+  beeper_status="$(upsc "${ups}" ups.beeper.status)"
   output_voltage="$(upsc "${ups}" output.voltage)"
-  output_volatage_nominal="$(upsc "${ups}" output.voltage.nominal)"
+  output_voltage_nominal="$(upsc "${ups}" output.voltage.nominal)"
   output_current="$(upsc "${ups}" output.current.nominal)"
   output_current_nominal="$(upsc "${ups}" output.current)"
-  input_frequency="$(upsc "${ups}" input.frequency)"
+  output_frequency="$(upsc "${ups}" output.frequency)"
+  output_frequency_nominal="$(upsc "${ups}" output.frequency.nominal)"
   input_voltage="$(upsc "${ups}" input.voltage)"
   input_current="$(upsc "${ups}" input.current)"
+  input_frequency="$(upsc "${ups}" input.frequency)"
   input_power="$(upsc "${ups}" input.power)"
   input_real_power="$(upsc "${ups}" input.realpower)"
-  battery_type="$(upsc "${ups}" battery.type)"
-  battery_runtime="$(upsc "${ups}" battery.runtime)"
   battery_charge="$(upsc "${ups}" battery.charge)"
   battery_voltage="$(upsc "${ups}" battery.voltage)"
   battery_voltage_nominal="$(upsc "${ups}" battery.voltage.nominal)"
   battery_capacity="$(upsc "${ups}" battery.capacity)"
   battery_current="$(upsc "${ups}" battery.current)"
   battery_temperature="$(upsc "${ups}" battery.temperature)"
+  battery_runtime="$(upsc "${ups}" battery.runtime)"
   battery_change_date="$(upsc "${ups}" battery.date)"
   battery_manufacturing_date="$(upsc "${ups}" battery.mfr.date)"
+  battery_type="$(upsc "${ups}" battery.type)"
 
   (
     echo "<b>UPS status report for ${ups}:</b>"
@@ -82,18 +82,18 @@ for ups in ${UPS_LIST}; do
     fi
 
     echo -e "\n<i>Status</i>"
+    if [[ -n "${uptime}" ]]; then
+      echo "Uptime: ${uptime} s"
+    fi
     echo "Status: ${status}"
+    if [[ -n "${temperature}" ]]; then
+      echo "Temperature: ${temperature} °C"
+    fi
     if [[ -n "${load}" ]]; then
       echo "Load: ${load} %"
     fi
     if [[ -n "${efficiency}" ]]; then
       echo "Efficiency: ${efficiency} %"
-    fi
-    if [[ -n "${uptime}" ]]; then
-      echo "Uptime: ${uptime} s"
-    fi
-    if [[ -n "${temperature}" ]]; then
-      echo "Temperature: ${temperature} °C"
     fi
 
     echo -e "\n<i>Input</i>"
@@ -103,28 +103,34 @@ for ups in ${UPS_LIST}; do
     if [[ -n "${input_current}" ]]; then
       echo "Input Current: ${input_current} A"
     fi
+    if [[ -n "${input_frequency}" ]]; then
+      echo "Input Frequency: ${input_frequency} Hz"
+    fi
     if [[ -n "${input_power}" ]]; then
       echo "Input Apparent Power: ${input_power} VA"
     fi
     if [[ -n "${input_real_power}" ]]; then
       echo "Input Real Power: ${input_real_power} W"
     fi
-    if [[ -n "${input_frequency}" ]]; then
-      echo "Input Frequency: ${input_frequency} Hz"
-    fi
 
     echo -e "\n<i>Output</i>"
     if [[ -n "${output_voltage}" ]]; then
       echo "Output Voltage: ${output_voltage} V"
     fi
-    if [[ -n "${output_volatage_nominal}" ]]; then
-      echo "Output Voltage (nominal): ${output_volatage_nominal} V"
+    if [[ -n "${output_voltage_nominal}" ]]; then
+      echo "Output Voltage (nominal): ${output_voltage_nominal} V"
     fi
     if [[ -n "${output_current}" ]]; then
       echo "Output Current: ${output_current} A"
     fi
     if [[ -n "${output_current_nominal}" ]]; then
       echo "Output Current (nominal): ${output_current_nominal} A"
+    fi
+    if [[ -n "${output_frequency}" ]]; then
+      echo "Output Frequency: ${output_frequency} Hz"
+    fi
+    if [[ -n "${output_frequency_nominal}" ]]; then
+      echo "Output Frequency (nominal): ${output_frequency_nominal} Hz"
     fi
     if [[ -n "${power}" ]]; then
       echo "Output Apparent Power: ${power} VA"
@@ -138,28 +144,10 @@ for ups in ${UPS_LIST}; do
     if [[ -n "${real_power_nominal}" ]]; then
       echo "Output Real Power (nominal): ${real_power_nominal} W"
     fi
-    if [[ -n "${output_frequency}" ]]; then
-      echo "Output Frequency: ${output_frequency} Hz"
-    fi
-    if [[ -n "${output_frequency_nominal}" ]]; then
-      echo "Output Frequency (nominal): ${output_frequency_nominal} Hz"
-    fi
 
     echo -e "\n<i>Battery</i>"
-    if [[ -n "${battery_type}" ]]; then
-      echo "Battery Type: ${battery_type}"
-    fi
-    if [[ -n "${battery_capacity}" ]]; then
-      echo "Battery Capacity: ${battery_capacity} Ah"
-    fi
-    if [[ -n "${battery_runtime}" ]]; then
-      echo "Battery Runtime: ${battery_runtime} s"
-    fi
     if [[ -n "${battery_charge}" ]]; then
       echo "Battery Charge: ${battery_charge} %"
-    fi
-    if [[ -n "${battery_temperature}" ]]; then
-      echo "Battery Temperature: ${battery_temperature} °C"
     fi
     if [[ -n "${battery_voltage}" ]]; then
       echo "Battery Voltage: ${battery_voltage} V"
@@ -170,22 +158,34 @@ for ups in ${UPS_LIST}; do
     if [[ -n "${battery_current}" ]]; then
       echo "Battery Current: ${battery_current} A"
     fi
+    if [[ -n "${battery_capacity}" ]]; then
+      echo "Battery Capacity: ${battery_capacity} Ah"
+    fi
+    if [[ -n "${battery_temperature}" ]]; then
+      echo "Battery Temperature: ${battery_temperature} °C"
+    fi
+    if [[ -n "${battery_runtime}" ]]; then
+      echo "Battery Runtime: ${battery_runtime} s"
+    fi
+    if [[ -n "${battery_type}" ]]; then
+      echo "Battery Type: ${battery_type}"
+    fi
 
     echo -e "\n<i>UPS Configuration</i>"
     if [[ -n "${beeper_status}" ]]; then
       echo "Beeper Status: ${beeper_status}"
-    fi
-    if [[ -n "${timer_shutdown}" ]]; then
-      echo "Shutdown Timer: ${timer_shutdown} s"
-    fi
-    if [[ -n "${timer_start}" ]]; then
-      echo "Start Timer: ${timer_start} s"
     fi
     if [[ -n "${delay_shutdown}" ]]; then
       echo "Shutdown Delay: ${delay_shutdown} s"
     fi
     if [[ -n "${delay_start}" ]]; then
       echo "Start Delay: ${delay_start} s"
+    fi
+    if [[ -n "${timer_shutdown}" ]]; then
+      echo "Shutdown Timer: ${timer_shutdown} s"
+    fi
+    if [[ -n "${timer_start}" ]]; then
+      echo "Start Timer: ${timer_start} s"
     fi
     if [[ -n "${test_interval}" ]]; then
       echo "Self-Test Interval: ${test_interval} s"
